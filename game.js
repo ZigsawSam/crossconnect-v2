1206c6c794bfbfbeec92d9309393f657968bcaa3
Md_Sameer_Ansari <61649538+ZigsawSam@users.noreply.github.com>
2026-03-16 10:55:49 +0530

    Update game.js

--- game.js
+++ game.js
@@ -432,10 +432,21 @@ function initPeer(hostId, connectTo) {
       debug: 0,
       config: {
         iceServers: [
-          { urls: 'stun:stun.l.google.com:19302' },
-          { urls: 'stun:stun1.l.google.com:19302' },
-          { urls: 'stun:stun2.l.google.com:19302' }
-        ]
+  { urls: "stun:stun.l.google.com:19302" },
+  { urls: "stun:stun1.l.google.com:19302" },
+  { urls: "stun:stun2.l.google.com:19302" },
+
+  {
+    urls: "turn:openrelay.metered.ca:80",
+    username: "openrelayproject",
+    credential: "openrelayproject"
+  },
+  {
+    urls: "turn:openrelay.metered.ca:443",
+    username: "openrelayproject",
+    credential: "openrelayproject"
+  }
+]
       }
     });
 
